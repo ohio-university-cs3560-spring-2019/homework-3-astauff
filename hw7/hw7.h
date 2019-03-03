0f83f627c824dbbd6c5ee3cbea3f2f5f6afe7e12
astauff <41757124+astauff@users.noreply.github.com>
2019-03-03 13:47:58 -0500

    Create hw7.h

---
+++ hw7/hw7.h
@@ -0,0 +1,13 @@
+#ifndef hw7_h
+#define hw7_h
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdarg>
+#include <cmath>
+#include <vector>
+using namespace std;
+
+double stddev(int n, ...);
+
+#endif
