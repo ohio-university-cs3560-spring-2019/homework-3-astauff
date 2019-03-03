dd4d4ec077ca4e90ef97565585de317de9c36de5
astauff <41757124+astauff@users.noreply.github.com>
2019-03-03 13:50:14 -0500

    Update hw7.h

--- hw7/hw7.h
+++ hw7/hw7.h
@@ -1,3 +1,14 @@
+//*********************************************
+//
+//  hw7.h
+//  Andrew Stauffer
+//  as211416@ohio.edu
+//  CS 3560
+//  This is the header file for the
+//  stddev function.
+//
+//*********************************************
+
 #ifndef hw7_h
 #define hw7_h
 
